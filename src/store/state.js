1e305314124954d3698ecbf14bbf4d07971ffe8d
Maisam11 <alimaisam0317@gmail.com>
2025-09-17 01:24:49 +0500

    remove unnecessary store of main store

--- src/store/state.js
+++
@@ -1,9 +0,0 @@
-// export default {
-//   users: [],
-//   formSubmissions: [],
-//   customData: [[]],
-//   customColumns: [[]],
-//   steps: [{ name: "Sheet 1", isEditing: false }],
-//   excelSubTab: 0,
-//   selectedRows: [],
-// };
